7d8c0c4c27ce06f0cea522e3c53cff9fd3d78578
Lauren Stephenson <compscilauren@gmail.com>
2019-04-06 11:36:30 -0500

    Prettier autochanges

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/*
-Author: Eric Seitz
-KUID: 2928468
-Assignment: Project 3
-Date: Mar 28 2019
-Class: EECS 448
-*/
-
-
 // Global Playfield Card
 let playFieldCard;
 
@@ -17,54 +8,54 @@ let players = [];
 let gameTurn = 0;
 
 //card constructor
-function card(color,value){
+function card(color, value) {
     this.color = color;
     this.value = value;
-    this.getColorValue = function(){
-        if(this.color == 'Red'){
+    this.getColorValue = function () {
+        if (this.color == 'Red') {
             return '#A60000';
-        }else if(this.color == 'Blue'){
+        } else if (this.color == 'Blue') {
             return '#2C0066';
-        }else if(this.color == 'Green'){
+        } else if (this.color == 'Green') {
             return '#004f19';
-        }else if(this.color == 'Yellow'){
+        } else if (this.color == 'Yellow') {
             return '#e5bf00';
-        }else{
+        } else {
             return '#333333';
         }
     }
 }
 
 //deck constructor
-function deck(){
+function deck() {
     this.cards = [];
     this.amtCards = 0;
 
     // Adds a card to the cards array
-    this.addCard = function(c){
+    this.addCard = function (c) {
         this.cards.push(c);
         this.amtCards = this.cards.length;
     };
 
 
     // removes a card from card array
-    this.removeCard = function(c){
+    this.removeCard = function (c) {
         this.cards.splice(c, 1);
         this.amtCards = this.cards.length;
     };
 
     // Gives player a random card
-    this.drawCard = function(){
+    this.drawCard = function () {
         let colorArray = ['Red', 'Green', 'Blue', 'Yellow'];
         let randColor = colorArray[Math.floor(Math.random() * colorArray.length)];
         let randValue = Math.floor((Math.random() * 10));
-        let tempCard = new card(randColor,randValue);
+        let tempCard = new card(randColor, randValue);
         this.addCard(tempCard);
         this.reloadHand();
     };
 
     //removes card from hand and reloads hand (post-validation of good move)
-    this.playCard = function(c){
+    this.playCard = function (c) {
         //Set playfield card to validated 'played' card
         playFieldCard.color = this.cards[c].color;
         playFieldCard.value = this.cards[c].value;
@@ -82,16 +73,16 @@ function deck(){
     };
 
     //Returns card at index c
-    this.getCard = function(c){
-        return(this.cards[c]);
+    this.getCard = function (c) {
+        return (this.cards[c]);
     };
 
     //Reloads the player hand to have the most recent cards in player hand
-    this.reloadHand = function(){
+    this.reloadHand = function () {
         let hand = document.getElementById('playerHand');
         hand.innerHTML = "";
         let i = 0;
-        for( i = 0; i < this.amtCards; i++){
+        for (i = 0; i < this.amtCards; i++) {
             let cardDiv = document.createElement('div');
             hand.append(cardDiv);
             cardDiv.innerHTML = this.getCard(i).value;
@@ -101,42 +92,38 @@ function deck(){
     };
 
     //For Testing. logs all cards and card amount
-    this.showDeck = function(){
-        for(i = 0; i < this.amtCards; i++){
+    this.showDeck = function () {
+        for (i = 0; i < this.amtCards; i++) {
             console.log(this.cards[i].color + " " + this.cards[i].value);
         }
         console.log("There are a total of " + this.amtCards + " in this deck");
     };
 
     //Compare selected card to playfield card
-    this.checkPlayerCardToPlayfield = function(c){
-    //Get in the value by element ID
-    let cardColor = this.cards[c].color;
-    let cardNumber = this.cards[c].value;
-    if (cardColor == playFieldCard.color)
-    {
-        return(true);
-    }
-    if (cardNumber == playFieldCard.value)
-    {
-        return(true);
-    }
+    this.checkPlayerCardToPlayfield = function (c) {
+        //Get in the value by element ID
+        let cardColor = this.cards[c].color;
+        let cardNumber = this.cards[c].value;
+        if (cardColor == playFieldCard.color) {
+            return (true);
+        }
+        if (cardNumber == playFieldCard.value) {
+            return (true);
+        }
 
-    return(false);
+        return (false);
     };//end of check card to playfield
 }
 
 
 //Testing function, plays a card
-function useCard()
-{
+function useCard() {
     //Get in the value by element ID
     let cardIndex = document.getElementById("cardIndex").value;
     //Validates the move is good (matching color/value)
     let isValidCard = players[gameTurn].playerDeck.checkPlayerCardToPlayfield(cardIndex);
     //Play card if valid move, otherwise ignore
-    if (isValidCard == true)
-    {
+    if (isValidCard == true) {
         alert("Debug: Valid move.");
         players[gameTurn].playerDeck.playCard(cardIndex);
         gameTurn++;
@@ -148,19 +135,17 @@ function useCard()
 
 
 //Changes the global card object to random color/value assignment
-function SelectPlayfieldCard()
-{
+function SelectPlayfieldCard() {
     let colorArray = ['Red', 'Green', 'Blue', 'Yellow'];
     let hexColor = ['#a60000', '#004F19', '#2C0066']
     let randColor = colorArray[Math.floor(Math.random() * colorArray.length)];
     let randValue = Math.floor((Math.random() * 10));
-    playFieldCard = new card(randColor,randValue);
+    playFieldCard = new card(randColor, randValue);
 }
 
 
 //Changes the displayed text and calls function to randomize playfield card
-function initializeWindow()
-{
+function initializeWindow() {
     //Get div elements that will be changed in HTML
     let divColor = document.getElementById('PlayfieldCardColor');
     let divValue = document.getElementById('PlayfieldCardValue');
@@ -174,52 +159,47 @@ function initializeWindow()
 }
 
 //Tracks and displays the current player  -- TRAVIS
-function playerTurn()
-{
-  if (gameTurn == players.length)
-    gameTurn = 0;
-  else if (gameTurn < 0)
-    gameTurn = players.length - 1;
+function playerTurn() {
+    if (gameTurn == players.length)
+        gameTurn = 0;
+    else if (gameTurn < 0)
+        gameTurn = players.length - 1;
 
-  let divPlayer = document.getElementById('playerID');
-  divPlayer.innerHTML = players[gameTurn].playerID;
-  players[gameTurn].playerDeck.reloadHand();
+    let divPlayer = document.getElementById('playerID');
+    divPlayer.innerHTML = players[gameTurn].playerID;
+    players[gameTurn].playerDeck.reloadHand();
 }
 
 //All players created, people and bots determined (future)  -- TRAVIS
-function initializePlayers()
-{
-  //Fills the players array with 2-4 people or bots (future, currently only allows two players)
-  while (players.length < 2)
-  {
-    let tempDeck = new deck();
-    let tempID = "";
-    while (tempID == "" || tempID == null)
-    {
-      tempID = prompt("Please enter your name.  If you would like to have a bot play for you, please enter the name 'Bot'");
+function initializePlayers() {
+    //Fills the players array with 2-4 people or bots (future, currently only allows two players)
+    while (players.length < 2) {
+        let tempDeck = new deck();
+        let tempID = "";
+        while (tempID == "" || tempID == null) {
+            tempID = prompt("Please enter your name.  If you would like to have a bot play for you, please enter the name 'Bot'");
+        }
+
+        let tempIndex = players.length - 1;
+        let tempPlayer = new player(tempDeck, tempID, tempIndex);
+
+        //Automatically gives the player 7 cards
+        let i = 0;
+        for (i = 0; i < 7; i++) { tempDeck.drawCard(); }
+
+        //adds the player to the game
+        players.push(tempPlayer);
     }
 
-    let tempIndex = players.length - 1;
-    let tempPlayer = new player(tempDeck, tempID, tempIndex);
-
-    //Automatically gives the player 7 cards
-    let i = 0;
-    for(i = 0; i< 7; i++){tempDeck.drawCard();}
-
-    //adds the player to the game
-    players.push(tempPlayer);
-  }
-
-  //Begins the first turn of the game
-  playerTurn();
+    //Begins the first turn of the game
+    playerTurn();
 }
 
 //Player constructor -- TRAVIS
-function player(deck, id, index)
-{
-  this.playerDeck = deck;
-  this.playerID = id;
-  this.playerIndex = index;
+function player(deck, id, index) {
+    this.playerDeck = deck;
+    this.playerID = id;
+    this.playerIndex = index;
 
 
 }
