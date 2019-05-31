2dd5282484fdf29a796b8aea6ebc3903c09f09d1
Lauren Stephenson <CompSciLauren@gmail.com>
2019-05-30 19:38:00 -0500

    Create drawCardAnimation function

--- cards.js
+++ cards.js
@@ -83,101 +83,13 @@ function Deck(divId, hidden) {
     let tempCard = new Card(randColor, randValue);
     this.addCard(tempCard);
 
-    //Draw Card Animation Start
     if (!initialDraw) {
-      //obtains drawPile div
-      let drawPile = document.getElementById("drawCardPile");
-
-      //create containers for the cards and add class
-      let drawCardContainer = document.createElement("div");
-      let drawCardContainerBack = document.createElement("div");
-      drawCardContainer.classList.add("drawCardContainer");
-      drawCardContainerBack.classList.add("drawCardContainer");
-
-      //append containers to drawpile div
-      drawPile.append(drawCardContainer);
-      drawPile.append(drawCardContainerBack);
-
-      //create card visuals
-      let cardDivBack = document.createElement("div");
-      let cardDiv = document.createElement("div");
-
-      //apend cards into containers
-      drawCardContainer.append(cardDiv);
-      drawCardContainerBack.append(cardDivBack);
-
-      //add class card to card divs
-      cardDiv.classList.add("card");
-      cardDivBack.classList.add("card");
-
-      //creates the inside of the cards
-      let cardSpan = document.createElement("span");
-      cardDiv.append(cardSpan);
-      cardSpan.classList.add("inner");
-
-      let cardSpanInner = document.createElement("span");
-      cardSpan.append(cardSpanInner);
-      cardSpanInner.classList.add("mark");
-
-      let cardSpanBack = document.createElement("span");
-      cardDivBack.append(cardSpanBack);
-      cardSpanBack.classList.add("inner");
-
-      let cardSpanInnerBack = document.createElement("span");
-      cardSpanBack.append(cardSpanInnerBack);
-      cardSpanInnerBack.classList.add("mark");
-
-      cardDiv.append();
-      cardDivBack.append();
-
-      addCSSDesignToBackOfCard(cardDivBack, cardSpanInnerBack);
-
-      if (this.hand.id == "BottomSeat") {
-        addCSSDesignToCard(cardDiv, cardSpanInner, randValue, randColor);
-
-        switch (randColor) {
-          case "Blue":
-            cardDiv.classList.add("blue");
-            break;
-          case "Red":
-            cardDiv.classList.add("red");
-            break;
-          case "Green":
-            cardDiv.classList.add("green");
-            break;
-          case "Yellow":
-            cardDiv.classList.add("yellow");
-            break;
-        }
-
-        drawCardContainer.classList.add("drawCardAnimationFrontDown");
-        drawCardContainerBack.classList.add("drawCardAnimationBack");
-      } else {
-        addCSSDesignToBackOfCard(cardDiv, cardSpanInner);
-
-        if (this.hand.id == "TopSeat") {
-          drawCardContainer.classList.add("drawCardAnimationHiddenUp");
-        } else if (this.hand.id == "RightSeat") {
-          drawCardContainer.classList.add("drawCardAnimationHiddenRight");
-        } else if (this.hand.id == "LeftSeat") {
-          drawCardContainer.classList.add("drawCardAnimationHiddenLeft");
-        } else {
-          drawCardContainer.classList.add("drawCardAnimationFront");
-        }
-        drawCardContainerBack.classList.add("drawCardBackHidden");
-      }
-
-      let thisObject = this;
-      setTimeout(function () {
-        drawPile.removeChild(drawPile.childNodes[0]);
-        drawPile.removeChild(drawPile.childNodes[0]);
-        thisObject.reloadHand();
-      }, 1000);
-    } else {
+      drawCardAnimation(this.hand.id, randValue, randColor, this);
+    }
+    else {
       this.reloadHand();
     }
 
-    //Draw Card Animation End
     console.log(
       players[gameTurn].playerID + " Drew a " + randColor + " " + randValue
     ); //testing
@@ -638,4 +550,94 @@ function addCSSDesignToCard(cardDiv, cardSpanInner, cardValue, cardColor) {
       cardSpanInner.append("⊘");
       break;
   }
+}
+
+function drawCardAnimation(thisHandId, randValue, randColor, thisObject) {
+  //obtains drawPile div
+  let drawPile = document.getElementById("drawCardPile");
+
+  //create containers for the cards and add class
+  let drawCardContainer = document.createElement("div");
+  let drawCardContainerBack = document.createElement("div");
+  drawCardContainer.classList.add("drawCardContainer");
+  drawCardContainerBack.classList.add("drawCardContainer");
+
+  //append containers to drawpile div
+  drawPile.append(drawCardContainer);
+  drawPile.append(drawCardContainerBack);
+
+  //create card visuals
+  let cardDivBack = document.createElement("div");
+  let cardDiv = document.createElement("div");
+
+  //apend cards into containers
+  drawCardContainer.append(cardDiv);
+  drawCardContainerBack.append(cardDivBack);
+
+  //add class card to card divs
+  cardDiv.classList.add("card");
+  cardDivBack.classList.add("card");
+
+  //creates the inside of the cards
+  let cardSpan = document.createElement("span");
+  cardDiv.append(cardSpan);
+  cardSpan.classList.add("inner");
+
+  let cardSpanInner = document.createElement("span");
+  cardSpan.append(cardSpanInner);
+  cardSpanInner.classList.add("mark");
+
+  let cardSpanBack = document.createElement("span");
+  cardDivBack.append(cardSpanBack);
+  cardSpanBack.classList.add("inner");
+
+  let cardSpanInnerBack = document.createElement("span");
+  cardSpanBack.append(cardSpanInnerBack);
+  cardSpanInnerBack.classList.add("mark");
+
+  cardDiv.append();
+  cardDivBack.append();
+
+  addCSSDesignToBackOfCard(cardDivBack, cardSpanInnerBack);
+
+  if (thisHandId == "BottomSeat") {
+    addCSSDesignToCard(cardDiv, cardSpanInner, randValue, randColor);
+
+    switch (randColor) {
+      case "Blue":
+        cardDiv.classList.add("blue");
+        break;
+      case "Red":
+        cardDiv.classList.add("red");
+        break;
+      case "Green":
+        cardDiv.classList.add("green");
+        break;
+      case "Yellow":
+        cardDiv.classList.add("yellow");
+        break;
+    }
+
+    drawCardContainer.classList.add("drawCardAnimationFrontDown");
+    drawCardContainerBack.classList.add("drawCardAnimationBack");
+  } else {
+    addCSSDesignToBackOfCard(cardDiv, cardSpanInner);
+
+    if (thisHandId == "TopSeat") {
+      drawCardContainer.classList.add("drawCardAnimationHiddenUp");
+    } else if (thisHandId == "RightSeat") {
+      drawCardContainer.classList.add("drawCardAnimationHiddenRight");
+    } else if (thisHandId == "LeftSeat") {
+      drawCardContainer.classList.add("drawCardAnimationHiddenLeft");
+    } else {
+      drawCardContainer.classList.add("drawCardAnimationFront");
+    }
+    drawCardContainerBack.classList.add("drawCardBackHidden");
+  }
+
+  setTimeout(function () {
+    drawPile.removeChild(drawPile.childNodes[0]);
+    drawPile.removeChild(drawPile.childNodes[0]);
+    thisObject.reloadHand();
+  }, 1000);
 }
